47742a47513d448b1b20e5ff6a597850575ca2ae
Jose Maria Sola Duran <josemaria.sola.duran@gmail.com>
2021-06-06 21:02:55 +0200

    Advance

--- lib/internal/objectdb_google_drive_storage.dart
+++ lib/internal/objectdb_google_drive_storage.dart
@@ -7,15 +7,14 @@ import 'package:googleapis/drive/v3.dart' as drive;
 import 'package:http/http.dart' as http;
 
 class GoogleDriveStorage extends FileSystemStorage {
-  final String _localPath;
+  final File _localFile;
   final String _drivePath;
   final drive.DriveApi _driveApi;
 
-  File _localFile;
   String _driveFileId;
 
   GoogleDriveStorage(
-      this._localPath, this._drivePath, http.Client googleAuthClient)
+      String _localPath, this._drivePath, http.Client googleAuthClient)
       : _driveApi = drive.DriveApi(googleAuthClient),
         _localFile = File(_localPath),
         super(_localPath);
@@ -24,9 +23,6 @@ class GoogleDriveStorage extends FileSystemStorage {
   Future<Meta> open([int version = 1]) async {
     var responseQuery = await _driveApi.files.list(q: "name='$_drivePath' and trashed = false");
     if (responseQuery.files.isNotEmpty) {
-      // FIXME: Download file if exists in remote Google Drive Storage
-      // driveFileContentStream.stream
-      
       _driveFileId = responseQuery.files[0].id;
       var driveFileContentStream = await _driveApi.files.get(_driveFileId, downloadOptions: drive.DownloadOptions.fullMedia) as drive.Media;
       if (await _localFile.exists()) {
@@ -34,7 +30,7 @@ class GoogleDriveStorage extends FileSystemStorage {
       }
       await for (var value in driveFileContentStream.stream) {
         await _localFile.writeAsBytes(value, mode: FileMode.append, flush: true);
-      }      
+      }
     }
     return super.open();
   }
@@ -60,14 +56,19 @@ class GoogleDriveStorage extends FileSystemStorage {
     return count;
   }
 
+  @override
+  Future cleanup() async {
+    await super.cleanup();
+    await _uploadGoogleDrive();
+  }
+
   Future<void> _uploadGoogleDrive() async {
     var driveFile = drive.File();
     driveFile.name = _drivePath;
 
     // Create a new back-up file on google drive.
-    var localFile = File(_localPath);
     var uploadMedia =
-        drive.Media(localFile.openRead(), await localFile.length());
+        drive.Media(_localFile.openRead(), await _localFile.length());
     if (_driveFileId != null) {
       await _driveApi.files
           .update(driveFile, _driveFileId, uploadMedia: uploadMedia);
